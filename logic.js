3cd9667551c54fc31efb227181dcee90a1f0741c
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-07-21 09:01:32 +0500

    Updated

--- logic.js
+++ logic.js
@@ -29,7 +29,7 @@ const quizData = [
 const Question = document.getElementById("question");
 const Buttons =  document.getElementById("row");
 const Nextbtn = document.getElementById("next");
-
+let button;
 let Index =0;
 let Score=0;
 
@@ -47,4 +47,54 @@ function Showquestion(params) {
 
   Question.innerHTML = 1+Index + ") " + quizData[Index].question;
   
+  //Very Important Concept for dynamic button and onclick on them
+  quizData[Index].options.forEach( value => {
+    button = document.createElement("button");
+    button.innerText = value;
+    button.classList.add("bt");
+    button.value = value;
+    Buttons.appendChild(button);
+    button.addEventListener("click", (e)=>{
+      Selectbtn(e.target);
+
+    });
+  })
 }
+
+function Selectbtn(selectedAnswer) {
+  // Disable all buttons to prevent further clicks
+  const allButtons = Buttons.querySelectorAll(".bt");
+  allButtons.forEach((button) => {
+    button.disabled = true;
+    button.cures
+  });
+
+  const correctAnswer = quizData[Index].correctAnswer;
+  const selectedValue = selectedAnswer.innerText;
+
+  if (selectedValue === correctAnswer) {
+    selectedAnswer.classList.add("correct");
+    Score++;
+  } else {
+    selectedAnswer.classList.add("Incorrect");
+    // Highlight the correct option
+    quizData[Index].options.forEach((value) => {
+      if (value === correctAnswer) {
+        const correctButton = Buttons.querySelector(`button[value="${value}"]`);
+        correctButton.classList.add("correct");
+      }
+    });
+  }
+  Nextbtn.style.display="block";
+}
+
+Nextbtn.addEventListener("click" ,()=>{
+  Index++;
+  Nextbtn.style.display="none";
+  while (Buttons.firstChild) {                    //imp concept of removing previous elements 
+    Buttons.removeChild(Buttons.firstChild);
+  }
+  Showquestion();
+
+
+})
